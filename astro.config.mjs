eaf10fbf7a05db0437ce7ff9009793fdafc42aa6
Conor Murray <64764695+programmurr@users.noreply.github.com>
2025-07-06 17:35:37 +0100

    Add island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://programmurr-astro-tutorial.netlify.app/",
+  integrations: [preact()],
 });
